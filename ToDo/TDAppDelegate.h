ed5072eac284cc5be5af7c911a557302d55e43f4
Gregory Casamento <greg.casamento@me.com>
2013-12-02 04:22:10 -0500

    Initial Commit

---
+++ ToDo/TDAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TDAppDelegate.h
+//  ToDo
+//
+//  Created by Gregory Casamento on 12/2/13.
+//  Copyright (c) 2013 Gregory Casamento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ToDo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDo
+//
+//  Created by Gregory Casamento on 12/2/13.
+//  Copyright (c) 2013 Gregory Casamento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDAppDelegate class]));
+    }
+}
